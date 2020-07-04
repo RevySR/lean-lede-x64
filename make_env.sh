db9c472e8ca3db7a09c753496fd07da1b13f409f
Rabenda <rabenda.cn@gmail.com>
2020-07-04 23:22:15 +0800

    Update Config & Fix ci.yml

--- make_env.sh
+++ make_env.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 set -e
-docker build . --network="host" -t lean_lede_build_env
+docker build . -f Dockerfile_local --network="host" -t lean_lede_build_env
 echo "Lean's LEDE Build env Done!"
-git clone https://github.com/coolsnowwolf/lede.git
-echo "LEDE Source Download Done!"
-cp config/.config lede/
-echo "Config Done!"
+if [ ! -d "lede" ] 
+then
+    git clone https://github.com/coolsnowwolf/lede.git
+    echo "LEDE Source Download Done!"
+    cp config/.config lede/
+    echo "Copy Old Config Done!"
+fi
+echo "Make env is Done!"
--- start_build.sh
+++ start_build.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
+dir=`pwd`
 docker run --network="host" --rm \
     --name lede_build \
-    -v /Volumes/lede_build/lede:/home/builder/lede \
+    -v $dir/lede:/home/builder/lede \
     -it lean_lede_build_env /bin/bash
